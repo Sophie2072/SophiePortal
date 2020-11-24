120572e93c79e0923d8dc1428ff6080e9cf7d7f9
Sophie2072 <72714965+Sophie2072@users.noreply.github.com>
2020-11-24 17:08:20 +1100

    update level on lonergan research

--- Sophie/try.py
+++ Sophie/try.py
@@ -1,6 +1,8 @@
-string = ""
+string = '[{"id":1,"start":"2020-01-01T12:00:00+00:00","end":"2020-01-01T16:00:00+00:00","extendedProps":{"is_final":false},"title":"#1: Job (4.00h)","notes":"","borderColor":"rgb(154,99,36)","backgroundColor":"rgba(154,99,36,0.75)"}'
 
 if not string == "":
     print("OKK")
 else:
     print("STRING is empty")
+
+print(string.decode("utf-8"))
--- notes.py
+++ notes.py
@@ -45,3 +45,35 @@ def get_sample_history(request, sample_id):
         ))
     else:
         return HttpResponse("No calls have been made to this number.")
+
+def test_time_filter(self):
+    query = models.Hour.objects.filter(start__gte="2020-01-01T08:00:00+00")\
+        .filter(end__lte="2020-01-02T8:00:00+00")\
+        .order_by("start")
+    self.assertStatusGET(
+        "timesheet_api_get_events",
+        200,
+        data={"start": "2020-01-01 8:00:00", "end": "2020-01-02 8:00:00"},
+    )
+    # self.get_content() 得到的是网页的response content，是 string ，
+    # 这里花了很长时间去compare格式，其实只要把strin 变成json就可以了，json.loads()
+    self.assertEqual(json.loads(self.get_content()), [x.as_json() for x in query])
+
+   def test_created_hour_is_correct(self):
+        self.assertStatusPOST(
+            "timesheet_api_create_event",
+            200,
+            data={
+                "start": "2020-01-01T12:00:00+00:00",
+                "end": "2020-01-01T16:00:00+00:00",
+                "job": self.job.id,
+                "task": self.task.id,
+            },
+        )
+        self.hour.refresh_from_db()  # This will update the existing hour object from the database, a cleaner way than using a query.
+        self.assertEqual(hour.job.name, self.job.name)
+        self.assertEqual(hour.task.name, self.task.name)
+        self.assertEqual(hour.start, datetime.datetime(2020, 1, 1, 12, 0, 0, tzinfo=pytz.utc))
+        self.assertEqual(hour.end, datetime.datetime(2020, 1, 1, 16, 0, 0, tzinfo=pytz.utc))
+
+

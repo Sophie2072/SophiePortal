2c9e69d0b6d49bc356712a336da535d8c1cd3194
Sophie2072 <72714965+Sophie2072@users.noreply.github.com>
2020-12-02 16:27:34 +1100

    update models field arrguement

--- Sophie/clio/models.py
+++ Sophie/clio/models.py
@@ -4,12 +4,17 @@ from django.utils import timezone
 
 class Staff(models.Model):
     forename = models.CharField(max_length=150)
-    surename = models.CharField(max_length=150)
+    surname = models.CharField(max_length=150)
     number = models.CharField("Mobile Number", max_length=15)
-    email = models.EmailField()
+    email = models.EmailField("Email")
     address = models.CharField(max_length=200, default="")
-
-    is_active = models.BooleanField(default=True)
+    team = models.ForeignKey(
+        "Team", on_delete=models.SET_NULL, null=True, related_name="staff"
+    )
+    role = models.ForeignKey(
+        "Role", on_delete=models.SET_NULL, null=True, related_name="staff"
+    )
+    is_active = models.BooleanField(default=False)
     date_joined = models.DateField(default=timezone.now)
     last_login = models.DateTimeField(default=timezone.now)
 
@@ -20,11 +25,17 @@ class Staff(models.Model):
 class Member(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     forename = models.CharField(max_length=150, default="")
-    surename = models.CharField(max_length=150,default="")
+    surname = models.CharField(max_length=150, default="")
     number = models.CharField("Mobile Number", max_length=15)
-    email = models.EmailField()
+    email = models.EmailField("Email")
     address = models.CharField(max_length=200, default="")
     balance = models.BigIntegerField(default=0)
+    first_card = models.ForeignKey(
+        "Card", on_delete=models.SET_NULL, null=True, related_name="first_card"
+    )
+    second_card = models.ForeignKey(
+        "Card", on_delete=models.SET_NULL, null=True, related_name="second_card"
+    )
     data_joined = models.DateField(default=True)
     notes = models.CharField(max_length=300, default="")
 
@@ -37,10 +48,10 @@ class Card(models.Model):
     value = models.BigIntegerField(default=0)
     cost = models.BigIntegerField(default=0)
     balance = models.BigIntegerField(default=0)
-    start = models.DateTimeField("start time", default="")
-    end = models.DateTimeField("end time", default="")
+    start = models.DateTimeField("Start Time", default="")
+    end = models.DateTimeField("End Time", default="")
     notes = models.CharField(max_length=300, default="")
-    date = models.DateTimeField('date published')
+    date = models.DateTimeField("Date Published")
 
     def __str__(self):
         return self.category
@@ -49,8 +60,9 @@ class Card(models.Model):
 class Team(models.Model):
     short_name = models.CharField(max_length=150)
     long_name = models.CharField(max_length=150)
-    is_hourly = models.BooleanField(default=True)
+    is_hourly = models.BooleanField(default=False)
 
 
 class Role(models.Model):
     title = models.CharField(max_length=150)
+    
